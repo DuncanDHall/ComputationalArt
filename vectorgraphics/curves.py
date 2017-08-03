ca1be5e35f346420ef9f9d9bfc5d0dda080381d6
Duncan Hall <duncan.hall@students.olin.edu>
2017-08-02 23:31:02 -0400

    can generate random curves. what's next?

--- vectorgraphics/curves.py
+++ vectorgraphics/curves.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 from matplotlib.path import Path
+import numpy as np
 
 def curve1():
 
@@ -74,15 +74,25 @@ def compound2():
     return verts, codes
 
 
-def gen_compound(num_curves):
+def gen_compound1(num_curves):
     # generates a compound set of 3 point curves which each share a
     # tangent at their terminus with the next.
     # arguments:
     #   num_curves - int number of sub curves
-    pass
 
-    verts = np.random.rand(1, num_curves * 2 + 1)
-    codes = [Path.MOVETO]
+    codes = [Path.MOVETO] + (num_curves * 2 * [Path.CURVE3])
+
+    verts = np.random.rand(num_curves * 2 + 1, 2)  # this is before "smoothing"
+    verts = verts[verts[:, 0].argsort()] # sorts according to x value only
+
+    # now smoothing occurs
+    for c in range(1, num_curves):
+        i = c * 2 + 1  # index of the middle point in sub curve 'c'
+        m = (verts[i-1][1] - verts[i-2][1]) / (verts[i-1][0] - verts[i-2][0])
+        dx = verts[i][0] - verts[i-1][0]
+        verts[i][1] = dx * m + verts[i-1][1]
+
+    return verts, codes
 
 
 
--- vectorgraphics/main.py
+++ vectorgraphics/main.py
@@ -2,17 +2,36 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
 import curves
+from tqdm import tqdm
 
 
-fig = plt.figure(figsize=[1,1])
-ax = fig.add_subplot(111)
+def plot_points(ax, verts):
+    ax.plot(verts[:,0], verts[:,1], 'x', lw=1, color='red', ms=10)
 
-path = Path(*curves.compound1())
-patch = patches.PathPatch(path, facecolor='none', lw=1)
-ax.add_patch(patch)
 
-ax.set_xlim(-0.1, 1.1)
-ax.set_ylim(-0.1, 1.1)
+def plot_curve(ax, verts, codes):
+    path = Path(verts, codes)
+    patch = patches.PathPatch(path, facecolor='none', lw=1)
+    ax.add_patch(patch)
 
-plt.axis('off')
-plt.savefig("test.svg")
+
+def make_figs(num):
+
+    for n in tqdm(range(num)):
+        fig = plt.figure(figsize=[6,10])
+        ax = fig.add_subplot(111)
+
+        verts, codes = curves.gen_compound1(3)
+        plot_curve(ax, verts, codes)
+        # plot_points(ax, verts)
+
+        ax.set_xlim(-0.1, 1.1)
+        ax.set_ylim(-0.5, 1.5)
+
+        plt.axis('off')
+        plt.savefig("out/curve{}.svg".format(n))
+        plt.close()
+
+
+if __name__ == "__main__":
+    make_figs(40)
